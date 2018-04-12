73ed2d2a6fc54ec3d39c4b5a46e491557d7b77ab
Alex Ramsdell <alexramsdell@gmail.com>
2018-04-11 17:30:44 -0700

    simple case is '?'

--- repl.js
+++ repl.js
@@ -1,23 +1,16 @@
 const r = require('repl');
-const prompt = '> ';
 const vm = require('vm');
+const PROMPT = 'node-help > ';
 
 function help(token) {
     return `help(${token})`;
 }
 
 function helpFilter(cmd, context, filename, callback) {
-
     let token = cmd.trim();
-    let output = token.endsWith('?') ? help(token.slice(0,-1)) : vm.runInThisContext(token) 
-
+    let helpFnCalled = token.endsWith('?');
+    let output = helpFnCalled ? help(token.slice(0,-1)) : vm.runInThisContext(token) 
     callback(null, output);
 }
 
-function writer(token) {
-
-    return token.endsWith('?') ? help(token) : token.slice(0,-1);
-
-}
-
-r.start({ prompt, eval: helpFilter });
+r.start({ prompt: PROMPT, eval: helpFilter });
