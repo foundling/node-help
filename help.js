6acca2017f0cf18f5e0a42a06d1031e02fe5b04d
Alex Ramsdell <alexramsdell@gmail.com>
2018-04-12 00:02:55 -0700

    notes in help.js

--- help.js
+++ help.js
@@ -1,5 +1,82 @@
-function help(cmdString) {
-    return `help(${cmdString})`; 
+const fs = require('fs');
+
+const keys = Object.keys;
+const docs = JSON.parse(fs.readFileSync('./docs/node/node-docs.json', 'utf8'));
+
+const { 
+    classes, 
+    globals, 
+    modules, 
+    methods 
+} = docs;
+
+const moduleProps = ['name','desc','modules','methods','type'];
+const isArray = x => x.constructor && x.constructor === Array;
+const isObject = x => x.constructor && x.constructor === Object;
+
+function processNode(node, target) {
+    if (node.name === target) {
+        moduleProps.forEach(t => console.log(`${t}: ${node[t]}`));
+    } 
 }
+
+function traverse(node) {
+    if (isArray(node)) {
+        for (let i = 0; i < node.length; ++i) {
+            traverse(node[i]);
+        }
+    } else if (isObject(node)) {
+        if (node.modules) {
+            traverse(node.modules);
+        }
+    }
+}; 
+
+
+function formatDocs({name, type, desc}) {
+    return `name: ${name}\ntype: ${type}\ndesc: ${desc}\n`;
+}
+
+function findGlobal(segment) {
+    let match = globals.filter(g => g.name === segment);
+    return (match.length) ? match[0] : null 
+}
+
+function help(token) {
+    const segments = token.split('.');
+    let match = 'no match';
+    if (segments.length === 1) {
+        match = findGlobal(segments[0]);
+    }
+
+    return formatDocs(match);
+}
+
+/* General Approach */
+
+/* Break the command int segments, splitting on '.' 
+ *
+ * taking the first path, search through: 
+ *      search globals, modules, classes, methods 
+ *      compare against 'name' property
+ *
+ * if no match, nothing found. no support for 'assert.ok' if 
+ * just 'ok' is entered. has to be fully qualified path.
+ *
+ * each time segment matches 'name' property in globals, modules, classes, methods 
+ * go to next path segment and keep digging down into the tree, comparing against child 'name' properties 
+ * if on last segment and 'name' matches segment name, get that object, pass to format
+ * otherwise: when no match for children, return 'not found'
+ *
+ */
+/* cases:
+ * help called on built by name: 
+ *  assert?
+ *  assert.ok.ok?
+ *
+ * help called on built in under different binding, e.g. B = Buffer, B?
+ *
+ *
+ */
 
 module.exports = exports = help;
--- repl.js
+++ repl.js
@@ -4,9 +4,17 @@ const vm = require('vm');
 const PROMPT = 'node-help > ';
 
 function helpFilter(cmd, context, filename, callback) {
-    let token = cmd.trim();
-    let helpFnCalled = token.endsWith('?');
-    let output = helpFnCalled ? help(token.slice(0,-1)) : vm.runInThisContext(token) 
+    // if there are 1 or more tokens that end with '?', concat help for each
+    // otherwise, just eval the line
+    let tokens = cmd.trim().split(' ');
+    let helpTokens = tokens.filter(t => t.endsWith('?'));
+    let output = null;
+
+    if (helpTokens.length)
+        console.log(helpTokens.map(t => help(t.slice(0,-1))).join('\n'));
+    else
+        output = vm.runInThisContext(cmd);
+
     callback(null, output);
 }
 
