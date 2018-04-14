7a4a4abf78627af15c3c6fb02f21ead79b416082
Alex Ramsdell <alexramsdell@gmail.com>
2018-04-13 18:17:44 -0700

    getMd module to get md from node website

--- utils.js
+++ utils.js
@@ -2,7 +2,8 @@ fs = require('fs');
 keys = Object.keys;
 flatten = a => a.reduce((acc, val) => acc.concat(val), []);
 docTree = JSON.parse(fs.readFileSync('docs/node/node-docs.json','utf8'));
-props = ['globals','methods','properties','modules','classes'];
+props = ['classes','events','globals','methods','modules'];  
+
 
 
 
