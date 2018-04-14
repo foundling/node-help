1bb802254bcf4318666d623444517f67ee01467b
Alex Ramsdell <alexramsdell@gmail.com>
2018-04-13 18:17:24 -0700

    account for namespaced node name for classes.  could be refactored

--- help.js
+++ help.js
@@ -8,7 +8,7 @@ const docTree = JSON.parse(fs.readFileSync('./docs/node/node-docs.json', 'utf8')
 
 const { classes, globals, modules, methods } = docTree;
 
-const moduleProps = ['name','desc','modules','methods','type'];
+const props = ['classes','events','globals','methods','modules']; 
 const isArray = x => x.constructor && x.constructor === Array;
 const isObject = x => x.constructor && x.constructor === Object;
 const equals = x => y => (x === y);
@@ -26,7 +26,6 @@ function formatDocs(node, containsModules=false) {
 
     if (node.signatures) {
         signatures = flatten(node.signatures.map(o => o.params.map(p => p.textRaw))).filter(Boolean);
-        console.log(signatures);
     }
     const sections = [
         `${chalk.bgBlue('Name:')} ${chalk.red(name)}`,
@@ -45,11 +44,13 @@ const flatten = a => a.reduce((acc, val) => acc.concat(val), []);
 
 function help(token) {
 
+    const containsModules = node => node && node.modules && node.modules.length;
     const segments = token.split('.');
-    const node = traverse(docTree, segments, 0);
-    const containsModules = node && node.modules && node.modules.length;
+    const nodes = traverse(docTree, segments, 0);
+    return nodes
+            .map(n => formatDocs(n, containsModules(n)))
+            .join('\n*******\n'); 
 
-    return formatDocs(node, containsModules);
 }
 
 
@@ -58,8 +59,6 @@ function help(token) {
 
 function traverse(root, segments, index) {
 
-    const props = ['globals','methods','properties','modules','classes']; 
-
     // get current segment
     let segment = segments[index];
 
@@ -67,11 +66,14 @@ function traverse(root, segments, index) {
     let children = flatten(props.map(key => root[key]).filter(Boolean));
 
     // keep children whose name property fuzzy matches the current segment
-    let targets = children.filter(c => c.name === segment || c.textRaw === segment || c.displayName === segment);
+    // NOTE: modules with classes have moduleName.className value for name property
+    let targets = children.filter(c => c.name === segment || c.textRaw === segment || c.displayName === segment || c.name.endsWith(`.${segment}`));
+
+    if (index == 1) console.log(children);
 
     if (index + 1 >= segments.length)
         // for now, return first match.
-        return targets[0];
+        return targets;
     else
         return traverse(targets[0], segments, index + 1);
 
