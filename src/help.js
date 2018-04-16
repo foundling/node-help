f1becc21a2bf2089378229311a02d8f34381bdd5
Alex Ramsdell <alexramsdell@gmail.com>
2018-04-15 19:33:30 -0700

    fix bug in findDocTrees

--- src/help.js
+++ src/help.js
@@ -5,35 +5,20 @@ const { nodeToDocString } = require(path.resolve(__dirname, 'format'));
 const { flatten, capitalize } = require(path.resolve(__dirname, 'utils'));
 
 
-function findDocTrees(docs) {
-
-    let mainTree = {
-        methods: docs.methods, 
-        methods: docs.modules, 
-        globals: docs.globals,
-        classes: docs.classes,
-    };
-
-    let errorsTree = docs.miscs.filter(node => node.name === 'Errors')[0];
-    let globalObjectsTree = docs.miscs.filter(node => node.name === 'Global Objects')[0];
-    
-    return [mainTree, errorsTree, globalObjectsTree].filter(Boolean);
-}
-
 function help(token, docs) {
 
     const docTrees = findDocTrees(docs);
     const segments = token.split('.');
     const treeResults = docTrees
         .map(tree => find(tree, segments, 0))
-    const docString = flatten(treeResults).map(node => nodeToDocString(node)).join('\n');
+    const docString = flatten(treeResults, depth=2).map(node => nodeToDocString(node)).join('\n');
 
     return docString;
 
 }
 
 function adjustSearchToken(token) {
-    // to account for globals weirdness in docs
+    // to account for inconsistency with globals in in api docs
 
     const toAdjust = ['process'];
 
@@ -49,6 +34,21 @@ function nameFilter(segment) {
     }
 }
 
+function findDocTrees(docs) {
+
+    let mainTree = {
+        methods: docs.methods, 
+        modules: docs.modules, 
+        globals: docs.globals,
+        classes: docs.classes,
+    };
+
+    let errorsTree = docs.miscs.filter(node => node.name === 'Errors')[0];
+    let globalObjectsTree = docs.miscs.filter(node => node.name === 'Global Objects')[0];
+    
+    return [mainTree, errorsTree, globalObjectsTree].filter(Boolean);
+}
+
 function find(root, segments, index) {
 
     const props = ['classes','events','globals','methods','miscs','modules','properties']; 
@@ -59,25 +59,14 @@ function find(root, segments, index) {
     // map root to target props values and flatten array
     let children = flatten(props.map(key => root[key]).filter(Boolean));
 
-    // keep children whose name property fuzzy matches the current segment
-    // NOTE: 
-    //  - modules with classes have moduleName.className value for name property
-    //  - 'process' module is in misc as 'Process'
-    let targets = children.filter(nameFilter(segment));
+    let matches = children.filter(nameFilter(segment));
 
-    /*
-    console.log('****');
-    console.log(segment);
-    console.log(targets);
-    console.log('****');
-    */
+    // if no matches or we've processed the last segment, we're done
+    if (!matches.length || index + 1 >= segments.length)
+        return matches;
 
-
-    // if targets is empty or if we're at last segment, then we're done
-    if (!targets.length || index + 1 >= segments.length)
-        return targets;
-
-    return find(targets[0], segments, index + 1);
+    // todo:return matches, not matches[0]
+    return find(matches, segments, index + 1);
 
 }
 
