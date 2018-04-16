8a9401fd61e5b50de9813899b597cd4d04744622
Alex Ramsdell <alexramsdell@gmail.com>
2018-04-15 18:45:28 -0700

    add module for formatting. correct process to Process when 'process?' command sent.

---
+++ src/format.js
@@ -0,0 +1,153 @@
+const chalk = require('chalk');
+const path = require('path');
+const striptags = require('striptags');
+const { flatten, decodeHTML } = require(path.resolve(__dirname, './utils'));
+const formatters = {
+
+    module: formatModule,
+    method: formatMethod,
+    property: formatProperty,
+    class: formatClass,
+    global: formatGlobal,
+    default: formatDefault
+
+};
+
+function nodeToDocString(node) {
+    if (!node)
+        return notFound(node);
+
+    if (node.type in formatters)
+        return formatters[node.type](node);
+    else
+        return formatters['default'](node);
+}
+
+function formatDefault(node) {
+
+    let signatures = [''];
+    const {name, type, desc} = node;
+
+    if (node.signatures)
+        signatures = flatten(node.signatures.map(o => o.params.map(p => p.textRaw))).filter(Boolean);
+
+    const sections = [
+        `${chalk.red('Name:')} ${name}`,
+        `${chalk.red('Type:')} ${type}`,
+        `${chalk.red('Signature(s):\n')}${node.textRaw + '\n' + signatures.join('\n')}`,
+        `${chalk.red('Description:\n')}${chalk.bgWhite.black(striptags(decodeHTML(desc)))}`,
+    ];
+
+    return sections.join('\n');
+}
+
+function formatModule(node) {
+
+    let signatures = [''];
+    const {name, type, desc} = node;
+
+    if (node.signatures)
+        signatures = flatten(node.signatures.map(o => o.params.map(p => p.textRaw))).filter(Boolean);
+
+    const sections = [
+        `${chalk.red('Name:')} ${name}`,
+        `${chalk.red('Type:')} ${type}`,
+        `${chalk.red('Signature(s):\n')}${node.textRaw + '\n' + signatures.join('\n')}`,
+        `${chalk.red('Description:\n')}${chalk.bgWhite.black(striptags(decodeHTML(desc)))}`,
+    ];
+
+    return sections.join('\n');
+
+}
+
+function formatMethod(node) {
+
+    let signatures = [''];
+    const {name, type, desc} = node;
+
+    if (node.signatures)
+        signatures = flatten(node.signatures.map(o => o.params.map(p => p.textRaw))).filter(Boolean);
+
+    const sections = [
+        `${chalk.red('Name:')} ${name}`,
+        `${chalk.red('Type:')} ${type}`,
+        `${chalk.red('Signature(s):\n')}${node.textRaw + '\n' + signatures.join('\n')}`,
+        `${chalk.red('Description:\n')}${chalk.bgWhite.black(striptags(decodeHTML(desc)))}`,
+    ];
+
+    return sections.join('\n');
+
+}
+
+function formatProperty(node) {
+
+    let signatures = [''];
+    const {name, type, desc} = node;
+
+    if (node.signatures)
+        signatures = flatten(node.signatures.map(o => o.params.map(p => p.textRaw))).filter(Boolean);
+
+    const sections = [
+        `${chalk.red('Name:')} ${name}`,
+        `${chalk.red('Type:')} ${type}`,
+        `${chalk.red('Signature(s):\n')}${node.textRaw + '\n' + signatures.join('\n')}`,
+        `${chalk.red('Description:\n')}${chalk.bgWhite.black(striptags(decodeHTML(desc)))}`
+    ];
+
+    return sections.join('\n');
+
+}
+
+function formatClass(node) {
+
+    let signatures = [''];
+    const {name, type, desc} = node;
+
+    if (node.signatures)
+        signatures = flatten(node.signatures.map(o => o.params.map(p => p.textRaw))).filter(Boolean);
+
+    const sections = [
+        `${chalk.red('Name:')} ${name}`,
+        `${chalk.red('Type:')} ${type}`,
+        `${chalk.red('Signature(s):\n')}${node.textRaw + '\n' + signatures.join('\n')}`,
+        `${chalk.red('Description:\n')}${chalk.bgWhite.black(striptags(decodeHTML(desc)))}`,
+    ];
+
+    return sections.join('\n');
+
+}
+
+function formatGlobal(node) {
+
+    let signatures = [''];
+    const {name, type, desc} = node;
+
+    if (node.signatures)
+        signatures = flatten(node.signatures.map(o => o.params.map(p => p.textRaw))).filter(Boolean);
+
+    const sections = [
+        `${chalk.red('Name:')} ${name}`,
+        `${chalk.red('Type:')} ${type}`,
+        `${chalk.red('Signature(s):\n')}${node.textRaw + '\n' + signatures.join('\n')}`,
+        `${chalk.red('Description:\n')}${chalk.bgWhite.black(striptags(decodeHTML(desc)))}`,
+    ];
+
+    return sections.join('\n');
+
+
+}
+
+function notFound(node) {
+    return `${chalk.bgBlue('No documentation found!')}`;
+}
+
+module.exports = exports = { 
+
+    nodeToDocString,
+    formatMethod,
+    formatProperty,
+    formatClass,
+    formatGlobal,
+    notFound,
+
+};
--- src/help.js
+++ src/help.js
@@ -1,28 +1,9 @@
 const fs = require('fs');
 const chalk = require('chalk');
 const path = require('path');
-const { capitalize, decodeHTML, flatten, keys, striptags  } = require(path.resolve(__dirname, 'utils'));
+const { nodeToDocString } = require(path.resolve(__dirname, 'format'));
+const { flatten, capitalize } = require(path.resolve(__dirname, 'utils'));
 
-function formatDocs(node, containsModules=false) {
-
-    if (!node)
-        return `${chalk.bgBlue('No documentation found!')}`;
-
-    const {name, type, desc} = node;
-    let signatures=[''];
-
-    if (node.signatures)
-        signatures = flatten(node.signatures.map(o => o.params.map(p => p.textRaw))).filter(Boolean);
-
-    const sections = [
-        `${chalk.bgBlue('Name:')} ${chalk.red(name)}`,
-        `${chalk.bgBlue('Type:')} ${chalk.red(type)}`,
-        `${chalk.bgBlue('Signature(s):\n')}${chalk.red(node.textRaw + '\n' + signatures.join('\n'))}`,
-        `${chalk.bgBlue('Description:\n')}${chalk.red(striptags(decodeHTML(desc)))}`,
-    ];
-
-    return sections.join('\n');
-}
 
 function findDocTrees(docs) {
 
@@ -42,14 +23,30 @@ function findDocTrees(docs) {
 function help(token, docs) {
 
     const docTrees = findDocTrees(docs);
-    const containsModules = node => node && node.modules && node.modules.length;
     const segments = token.split('.');
     const treeResults = docTrees
         .map(tree => find(tree, segments, 0))
-    const docString = flatten(treeResults).map(node => formatDocs(node, containsModules(node))).join('\n');
+    const docString = flatten(treeResults).map(node => nodeToDocString(node)).join('\n');
 
     return docString;
 
+}
+
+function adjustSearchToken(token) {
+    // to account for globals weirdness in docs
+
+    const toAdjust = ['process'];
+
+    if (toAdjust.includes(token))
+        return capitalize(token);
+
+    return token;
+}
+
+function nameFilter(segment) {
+    return function(node) {
+        return node.name === segment || node.textRaw === segment || node.displayName === segment || node.name.endsWith(`.${segment}`);
+    }
 }
 
 function find(root, segments, index) {
@@ -57,7 +54,7 @@ function find(root, segments, index) {
     const props = ['classes','events','globals','methods','miscs','modules','properties']; 
 
     // get current segment
-    let segment = segments[index];
+    let segment = adjustSearchToken(segments[index]);
 
     // map root to target props values and flatten array
     let children = flatten(props.map(key => root[key]).filter(Boolean));
@@ -66,7 +63,15 @@ function find(root, segments, index) {
     // NOTE: 
     //  - modules with classes have moduleName.className value for name property
     //  - 'process' module is in misc as 'Process'
-    let targets = children.filter(c => c.name === segment || c.textRaw === segment || c.displayName === segment || c.name.endsWith(`.${segment}`));
+    let targets = children.filter(nameFilter(segment));
+
+    /*
+    console.log('****');
+    console.log(segment);
+    console.log(targets);
+    console.log('****');
+    */
+
 
     // if targets is empty or if we're at last segment, then we're done
     if (!targets.length || index + 1 >= segments.length)
