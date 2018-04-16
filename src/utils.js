06260ab719f061e359af437db5c1a7e46e5c8814
Alex Ramsdell <alexramsdell@gmail.com>
2018-04-15 17:52:35 -0700

    make flatten recursive.

--- src/utils.js
+++ src/utils.js
@@ -4,8 +4,13 @@ const Entities = require('html-entities');
 
 const capitalize = s => s[0].toUpperCase() + s.slice(1).toLowerCase();
 const { decode } = new Entities.AllHtmlEntities();
-const flatten = a => a.reduce((acc, val) => acc.concat(val), []);
+//const flatten = a => a.reduce((acc, val) => acc.concat(val), []);
 const rmLastChar = s => s.slice(0,-1);
+
+function flatten(a, depth=1) {
+
+    return depth <= 0 ? a : flatten(a.reduce((acc, val) => acc.concat(val), []), depth - 1);
+};
 
 function clear() {
     'use strict';
