be18d36302ef29b6de8d341708be39b4b1b57eb7
Alex Ramsdell <alexramsdell@gmail.com>
2018-04-15 17:52:26 -0700

    in order to take into account the confusing structure of the json docs, break tree into subtrees that have congruent structure.

--- src/help.js
+++ src/help.js
@@ -11,32 +11,50 @@ function formatDocs(node, containsModules=false) {
     const {name, type, desc} = node;
     let signatures=[''];
 
-    if (node.signatures) {
+    if (node.signatures)
         signatures = flatten(node.signatures.map(o => o.params.map(p => p.textRaw))).filter(Boolean);
-    }
+
     const sections = [
         `${chalk.bgBlue('Name:')} ${chalk.red(name)}`,
         `${chalk.bgBlue('Type:')} ${chalk.red(type)}`,
         `${chalk.bgBlue('Signature(s):\n')}${chalk.red(node.textRaw + '\n' + signatures.join('\n'))}`,
         `${chalk.bgBlue('Description:\n')}${chalk.red(striptags(decodeHTML(desc)))}`,
     ];
+
     return sections.join('\n');
 }
 
-function help(token, docTree) {
+function findDocTrees(docs) {
 
+    let mainTree = {
+        methods: docs.methods, 
+        methods: docs.modules, 
+        globals: docs.globals,
+        classes: docs.classes,
+    };
+
+    let errorsTree = docs.miscs.filter(node => node.name === 'Errors')[0];
+    let globalObjectsTree = docs.miscs.filter(node => node.name === 'Global Objects')[0];
+    
+    return [mainTree, errorsTree, globalObjectsTree].filter(Boolean);
+}
+
+function help(token, docs) {
+
+    const docTrees = findDocTrees(docs);
     const containsModules = node => node && node.modules && node.modules.length;
     const segments = token.split('.');
-    const nodes = traverse(docTree, segments, 0);
-    return nodes
-            .map(n => formatDocs(n, containsModules(n)))
-            .join('\n*******\n'); 
+    const treeResults = docTrees
+        .map(tree => find(tree, segments, 0))
+    const docString = flatten(treeResults).map(node => formatDocs(node, containsModules(node))).join('\n');
+
+    return docString;
 
 }
 
-function traverse(root, segments, index) {
+function find(root, segments, index) {
 
-    const props = ['classes','events','globals','methods','modules']; 
+    const props = ['classes','events','globals','methods','miscs','modules','properties']; 
 
     // get current segment
     let segment = segments[index];
@@ -45,15 +63,17 @@ function traverse(root, segments, index) {
     let children = flatten(props.map(key => root[key]).filter(Boolean));
 
     // keep children whose name property fuzzy matches the current segment
-    // NOTE: modules with classes have moduleName.className value for name property
+    // NOTE: 
+    //  - modules with classes have moduleName.className value for name property
+    //  - 'process' module is in misc as 'Process'
     let targets = children.filter(c => c.name === segment || c.textRaw === segment || c.displayName === segment || c.name.endsWith(`.${segment}`));
 
-    if (index + 1 >= segments.length)
-        // for now, return first match.
+    // if targets is empty or if we're at last segment, then we're done
+    if (!targets.length || index + 1 >= segments.length)
         return targets;
-    else
-        return traverse(targets[0], segments, index + 1);
+
+    return find(targets[0], segments, index + 1);
 
 }
 
-module.exports = exports = { help, traverse };
+module.exports = exports = { help, find };
