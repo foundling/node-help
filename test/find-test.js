8cfe589807d7c66e22677d07a435466d120cdfbf
Alex Ramsdell <alexramsdell@gmail.com>
2018-04-18 13:04:54 -0700

    use path.join over path.resolve

--- test/find-test.js
+++ test/find-test.js
@@ -1,21 +1,10 @@
 const fs = require('fs');
 const test = require('tape');
 const path = require('path'); 
-const { find } = require(path.resolve(path.join(__dirname,'..','src','help')));
-const { keys, flatten } = require(path.resolve(path.join(__dirname,'..','src','utils')));
-const docsPath = path.resolve(__dirname,'..','docs','node','node-all.json');
+const { find, findAllNames } = require(path.join(__dirname,'..','src','help'));
+const { keys, flatten } = require(path.join(__dirname,'..','src','utils'));
+const docsPath = path.join(__dirname,'..','docs','node','node-all.json');
 
-function findAllNames(tree) {
-
-    const props = ['modules','methods','classes','globals','properties','miscs'];    
-    const children = flatten(props.map(key => tree[key]).filter(Boolean));
-    const names = children.map(child => child.name);
-    
-    if (children.length)
-        return flatten(names.concat(children.map(findAllNames)));
-    else 
-        return names;
-}
 
 test('lookup', function(t) {
 
@@ -27,7 +16,9 @@ test('lookup', function(t) {
         const docs = JSON.parse(data);
         const names = findAllNames(docs);
         const deduped = Array.from(new Set(names)); 
+
         console.log(deduped);
+
         t.equal(Array.isArray(deduped), true); 
         t.end();
 
