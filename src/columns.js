90bb7e8587f131ee9de9af11434ceab2f80587ff
Alex Ramsdell <alexramsdell@gmail.com>
2018-04-18 17:13:19 -0700

    clean up columns.js

--- src/columns.js
+++ src/columns.js
@@ -1,4 +1,8 @@
-function splitBy(items, n) {
+const max = ns => ns.reduce((acc, n) => n > acc ? n : acc,0);
+const sum = items => items.reduce((acc,x) => acc + x, 0);
+const longest = items => max(items.map(i => i.length));
+
+function subdivide(items, n) {
 
     let i = 0;
     let next;
@@ -17,8 +21,6 @@ function splitBy(items, n) {
 
 }
 
-const max = ns => ns.reduce((acc, n) => n > acc ? n : acc,0);
-const longest = items => max(items.map(i => i.length));
 
 function zipLongest(...seqs) {
     let results = [];
@@ -50,67 +52,24 @@ function padRight(s, max) {
 } 
 
 function columnize(items, sep='  ') {
+
     items.sort();
+
     const longestWord = longest(items) + sep.length;
     const width = process.stdout.columns;
     const wordsPerLine = Math.floor(width/longestWord);
     const lineCount = Math.ceil(items.length/wordsPerLine);
     const rows = items;
-    return zipLongest(...splitBy(items, lineCount))
-            .map(row => row.map(item => padRight(item, longestWord)).join('')).join('\n');
+
+    return zipLongest(...subdivide(items, lineCount))
+            .map(row => {
+                return row
+                        .map(item => padRight(item, longestWord))
+                        .join('')
+            })
+            .join('\n');
 }
 
-function sum(items) {
-    return items.reduce((acc,x) => acc + x, 0);
-}
-
-function wrap(props) {
-    const cols = process.stdout.columns;
-    const termWidth = cols < 80 ? cols : 80;
-    const rows = [[]];
-    let len = 0;
-    let rowIndex = 0;
-    for (let i = 0; i < props.length; ++i) {
-        if (sum(rows[rowIndex].map(item => (item + ' ').length)) + props[i].length >= termWidth)
-            rows[++rowIndex] = [];
-        rows[rowIndex].push(props[i]);
-    }
-    return '\n' + rows.map(row => row.join(' ')).join('\n');
-}
-
-
-
-function combinedLength(items) {
-    return items.reduce((acc, item) => {
-        return acc + (item + '\t').length;
-    },0);
-}
-
-
-let items =  [
-  'connecting',
-  '_hadError',
-  '_handle',
-  '_parent',
-  '_host',
-  '_readableState',
-  'readable',
-  'domain',
-  '_events',
-  '_eventsCount',
-  '_maxListeners',
-  '_writableState',
-  'writable',
-  '_bytesDispatched',
-  '_sockname',
-  '_writev',
-  '_pendingData',
-  '_pendingEncoding',
-  'allowHalfOpen',
-  'server',
-  '_server',
-  'isRaw',
-  'isTTY',
-  'fd' 
-];
-console.log(columnize(items));
+module.exports = exports = { 
+    columnize 
+};
