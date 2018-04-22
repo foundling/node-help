443a0425b1b3df51419eebe42037cc852d58da77
Alex Ramsdell <alexramsdell@gmail.com>
2018-04-22 12:53:40 -0700

    correct behavior of zip longest

--- test/columns-test.js
+++ test/columns-test.js
@@ -25,6 +25,6 @@ test('zipLongest', function(t) {
     t.deepEqual(r1, [[1,5,9], [2,6,10], [3,7,11], [4,8]] );
 
     let r2 = zipLongest( [1,2,3] );  
-    t.deepEqual(r2, [[1,2,3]]);
+    t.deepEqual(r2, [[1],[2],[3]]);
 
 });
