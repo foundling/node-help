ce311a77dddb53f0d6ac5bdb1bc0afdf036fd664
Alex Ramsdell <alexramsdell@gmail.com>
2018-04-22 16:13:00 -0700

    remove unused requires.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-const fs = require('fs');
 const path = require('path');
-const util = require('util');
 const chalk = require('chalk');
-const touch = require('touch');
-const { homedir } = require('os');
 
 const repl = require(path.join(__dirname, 'repl')); 
 const { progInfo } = require(path.join(__dirname, 'format'));
-const { clear, readFilePromise } = require(path.join(__dirname, 'utils'));
 const { main }  = require(path.join(__dirname, 'init'));
-const bannerPath = path.join(__dirname, 'banner.txt'); 
-const nodeDocsJSON = path.join(__dirname,'..','src','docs','node','node-all.json');
 
 function init() {
 
